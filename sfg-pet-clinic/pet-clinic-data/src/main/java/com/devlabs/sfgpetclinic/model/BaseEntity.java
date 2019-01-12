926dc70a00e5e97605b4b23033ebff7ab43ae1e0
Deepak Bhagat <deepak891@gmail.com>
2019-01-12 21:49:41 +0800

    Added attribute Id in model. closes #10

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/devlabs/sfgpetclinic/model/BaseEntity.java
@@ -0,0 +1,16 @@
+package com.devlabs.sfgpetclinic.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public BaseEntity setId(Long id) {
+        this.id = id;
+        return this;
+    }
+}
--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/devlabs/sfgpetclinic/model/Person.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/devlabs/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.devlabs.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
