6350c581d6382d2cc77e911998df7ff66e0342e2
Deepak Bhagat <deepak891@gmail.com>
2019-01-16 21:01:58 +0800

    Implemented vets list. Close.  #14

--- sfg-pet-clinic/pet-clinic-web/src/main/java/com/devlabs/sfgpetclinic/controller/VetController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/com/devlabs/sfgpetclinic/controller/VetController.java
@@ -1,13 +1,22 @@
 package com.devlabs.sfgpetclinic.controller;
 
+import com.devlabs.sfgpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/vet-list","/vets/index", "/vets/index.html"})
-    public String listOfVets(){
+    public String listOfVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/vet-list";
     }
 }
