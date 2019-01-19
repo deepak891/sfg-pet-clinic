109f9416536a7a6562b2bdb03cc9f08cad1a8c1d
Deepak Bhagat <deepak891@gmail.com>
2019-01-19 14:38:50 +0800

    Created pet type Map service. Close #29

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/devlabs/sfgpetclinic/service/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.devlabs.sfgpetclinic.service;
+
+import com.devlabs.sfgpetclinic.model.PetType;
+import com.devlabs.sfgpetclinic.service.CurdService;
+
+public interface PetTypeService extends CurdService<PetType, Long> {
+}
