3093d69495823c96d2bd951e463cbeee8c5fe3e4
Deepak Bhagat <deepak891@gmail.com>
2019-01-20 09:25:33 +0800

    Fixed borken links. Close #45

--- sfg-pet-clinic/pet-clinic-web/src/main/java/com/devlabs/sfgpetclinic/controller/OwnerController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/com/devlabs/sfgpetclinic/controller/OwnerController.java
@@ -20,4 +20,9 @@ public class OwnerController {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/owners";
     }
+
+    @RequestMapping({"/find"})
+    public String findOwners(){
+        return "notImplemented";
+    }
 }
