c249edb2e4338e59def605e13ed9cdf4a22a1a19
Deepak Bhagat <deepak891@gmail.com>
2019-01-27 15:45:00 +0800

    Added project Lombok and Refactor. Close #51

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/devlabs/sfgpetclinic/model/Person.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/devlabs/sfgpetclinic/model/Person.java
@@ -1,8 +1,17 @@
 package com.devlabs.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity{
     @Column(name = "first_name")
@@ -10,21 +19,9 @@ public class Person extends BaseEntity{
     @Column(name = "last_name")
     private String lastName;
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public Person setFirstName(String firstName) {
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
         this.firstName = firstName;
-        return this;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public Person setLastName(String lastName) {
         this.lastName = lastName;
-        return this;
     }
 }
--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/devlabs/sfgpetclinic/model/Pet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/devlabs/sfgpetclinic/model/Pet.java
@@ -1,10 +1,17 @@
 package com.devlabs.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity{
@@ -25,47 +32,4 @@ public class Pet extends BaseEntity{
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public PetType getPetType() {
-        return petType;
-    }
-
-    public Pet setPetType(PetType petType) {
-        this.petType = petType;
-        return this;
-    }
-
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public Pet setOwner(Owner owner) {
-        this.owner = owner;
-        return this;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public Pet setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-        return this;
-    }
-
-    public Set<Visit> getVisits() {
-        return visits;
-    }
-
-    public void setVisits(Set<Visit> visits) {
-        this.visits = visits;
-    }
 }
